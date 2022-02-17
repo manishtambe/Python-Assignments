90f850967882e1106f169faaf72a912b81150b81
Manish Tambe <52567320+manishtambe@users.noreply.github.com>
2022-02-17 22:31:51 +0530

    Add files via upload

---
+++ Assignment No.3/Codes/Classes.py
@@ -0,0 +1,50 @@
+class Student:
+    id = 0
+    name = ""
+    age = 0
+    m1, m2, m3 = 0, 0, 0
+    total = 0
+    percentage = 0.0
+    status = ""
+    grade = ""
+
+    def AcceptInput(self):
+        self.id = int(input("Enter the id of the student = "))
+        self.name = input("Enter The Student FullName = ")
+        self.age = int(input("Enter The Age Of The Student = "))
+        self.m1 = int(input("Enter The Student Marks In Maths Subject Out Of 100= "))
+        self.m2 = int(input("Enter The Student Marks In Science Subject Out Of 100= "))
+        self.m3 = int(input("Enter The Student Marks In English Subject Out Of 100= "))
+
+    def Calculate(self):
+        self.total = self.m1 + self.m2 + self.m3
+        self.percentage = ((self.total * 100) / 300)
+
+        if self.percentage >= 75:
+            self.grade = "A"
+            self.status = "Pass"
+        elif 60 <= self.percentage < 75:
+            self.grade = "B"
+            self.status = "Pass"
+        elif 50 <= self.percentage < 60:
+            self.grade = "C"
+            self.status = "Pass"
+        else:
+            self.grade = "No Grade"
+            self.status = "Fail"
+
+        print("Student Id: ", self.id)
+        print("Student Name: ", self.name)
+        print("Student Age: ", self.age)
+        print("Student Marks In Maths = ", self.m1)
+        print("Student Marks In Science = ", self.m2)
+        print("Student Marks In English = ", self.m3)
+        print("Student Total Marks = ", self.total)
+        print("Student Percentage = ", self.percentage)
+        print("Student Status = ", self.status)
+        print("Student Grade = ", self.grade)
+
+
+Obj = Student()
+Obj.AcceptInput()
+Obj.Calculate()
